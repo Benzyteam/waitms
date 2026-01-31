fd021ebdb97cfc6f03bfbf6f13093a6648a0e8f9
ashishKumarbenzy <165278661+ashishKumarbenzy@users.noreply.github.com>
2026-01-31 16:05:09 +0530

    Reduce sleep duration in wait_api from 40 to 25 seconds

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.post("/wait")
 async def wait_api(request: Request):
     kwargs = await request.json()
-    await asyncio.sleep(40)
+    await asyncio.sleep(25)
     
     return {"data": kwargs}
 
